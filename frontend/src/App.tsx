0f3fe6dc906ab28a4584d9d766020697e058a45e
ugurcan <ugurcan.turk@etstur.com>
2025-08-18 17:11:06 +0300

    css sayfa boyu düzenlendi

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,14 +7,16 @@ import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
 
 export const App = () => {
   return (
-    <>
+    <div className="d-flex flex-column min-vh-100">
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Navigate to="/home" />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/search" element={<SearchBooksPage />} />
-      </Routes>
+      <div className="flex-grow-1">
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/search" element={<SearchBooksPage />} />
+        </Routes>
+      </div>
       <Footer />
-    </>
+    </div>
   );
 };
